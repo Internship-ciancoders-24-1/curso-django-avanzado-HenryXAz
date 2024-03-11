5c10087b365ffeb6af2b74fb2a7ac4f8d463fbaa
Henry Gustavo X Az <azhenrygus@outlook.es>
2024-03-11 12:12:11 -0600

    Dia 7:capitulo 7 del curso avanzado de django

--- proyecto/cride/circles/urls.py
+++ proyecto/cride/circles/urls.py
@@ -8,9 +8,15 @@ from rest_framework.routers import DefaultRouter
 
 # views
 from cride.circles.views import circles as circle_views
+from cride.circles.views.memberships import MembershipViewSet
 
 router = DefaultRouter()
-router.register(r'circles', circle_views.CircleViewSet, basename='circle')
+# router.register(r'circles', circle_views.CircleViewSet, basename='circle')
+router.register(
+  r'circles/(?P<slug_name>[-a-zA-Z0-0_]+)/members',
+  MembershipViewSet,
+  basename='membership'
+)
 
 urlpatterns = [
     path('', include(router.urls)),
